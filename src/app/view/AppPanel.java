858aa4f3827680404bed17328365d97d8e5b0c05
Sam Montoya <samuel.montoya@080n-pdc-09.canyonsdistrict.pvt>
2016-06-23 14:08:26 -0600

    The application actually works with its logic!

--- src/app/view/AppPanel.java
+++ src/app/view/AppPanel.java
@@ -1,5 +1,6 @@
 package app.view;
 
+import java.awt.Color;
 import java.awt.List;
 import java.awt.event.*;
 import javax.swing.*;
@@ -41,6 +42,7 @@ public class AppPanel extends JPanel
 		layout.putConstraint(SpringLayout.SOUTH, switchList, -375, SpringLayout.SOUTH, this);
 		switchList.add("5500 Switch");
 		switchList.add("5800 Switch");
+		switchList.select(0);
 
 		// Auto Select 48 Port
 		fortyPortButton = new JRadioButton("48 Port", true);
@@ -55,7 +57,7 @@ public class AppPanel extends JPanel
 		}
 
 		userDomain = new JFormattedTextField();
-		userDomain.setValue(new Integer(domainNum));
+		userDomain.setText("10");
 		
 		radioButtons = new ButtonGroup();
 		radioButtons.add(this.fortyPortButton);
@@ -77,8 +79,6 @@ public class AppPanel extends JPanel
 		stackLabel = new JLabel("Select the position in the stack");
 		domainLabel = new JLabel("Type Domain (Numbers Only)");
 
-		updateTextCode("5800", 48, 1, 10, 32);
-
 		setupChatPane();
 		buildPanel();
 		buildPlacements();
@@ -90,20 +90,43 @@ public class AppPanel extends JPanel
 		codePane.setLineWrap(true);
 		codePane.setWrapStyleWord(true);
 		textScrollPane = new JScrollPane(codePane);
+		layout.putConstraint(SpringLayout.EAST, textScrollPane, -20, SpringLayout.EAST, this);
 		textScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		textScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 	}
 
-	private void updateTextCode(String switchC, int portC, int positionC, int domainC, int priorityC)
+	private void updateTextCode(int positionC, String domainC, int priorityC, int interfaceInt)
 	{
+		int interfaceInt2 = interfaceInt + 1;
 		this.codePane
-				.setText("sys" + "\nirf domain " + domainC + "\nirf member " + positionC + " priority " + priorityC);
+				.setText("sys"
+		+ "\nirf domain " + domainC + "\nirf member " + positionC + " priority " + priorityC 
+		+ "\nInterface Ten " + positionC + "/0/" +  interfaceInt
+		+ "\nshut"
+		+ "\nInterface Ten " + positionC + "/0/" +  interfaceInt2
+		+ "\nshut" 
+		+ "\nirf-port " + positionC + "/1"
+		+ "\nport group interface ten " + positionC + "/0/" +  interfaceInt + " mode enhanced"
+		+ "\nirf-port " + positionC + "/2"
+		+ "\nport group interface ten " + positionC + "/0/" +  interfaceInt2 + " mode enhanced"
+		+ "\nInterface Ten " + positionC + "/0/" +  interfaceInt
+		+ "\nundo shut"
+		+ "\nInterface Ten " + positionC + "/0/" + interfaceInt2
+		+ "\nundo shut"
+		+ "\nqu"
+		+ "\nsave");
 	}
 
+	private boolean selectionChecker()
+	{
+		boolean isFilledOut = true;
+		return isFilledOut;
+	}
+	
 	private void buildPanel()
 	{
-
 		setLayout(this.layout);
+		this.setBackground(Color.WHITE);
 		add(this.switchList);
 		add(this.switchNumbers);
 		add(this.fortyPortButton);
@@ -125,7 +148,40 @@ public class AppPanel extends JPanel
 		{
 			public void actionPerformed(ActionEvent clicked)
 			{
-				
+				if(selectionChecker() == true)
+				{
+					// int positionC, int domainC, int priorityC, int interfaceInt
+					int interfaceInt = 53;
+					String domainC = userDomain.getText();
+					String switchName = switchList.getSelectedItem();
+					int positionC = switchNumbers.getSelectedIndex() + 1;
+					
+					int priority = 32;
+					int newPriority = priority;
+					
+					for(int counter = 1; counter <= 9; counter++)  
+					{
+						if (positionC == counter) 
+						{
+							priority = newPriority;
+						}
+						else
+						{
+							newPriority = priority - 2;
+						}
+					}
+					
+					if(switchName.contains("5800") && fortyPortButton.isSelected())
+						interfaceInt = 51;
+					else if(switchName.contains("5800") && twentyPortButton.isSelected())
+						interfaceInt = 25;
+					else if(switchName.contains("5500") && fortyPortButton.isSelected())
+						interfaceInt = 53;
+					else if(switchName.contains("5500") && twentyPortButton.isSelected())
+						interfaceInt = 29;
+					
+					updateTextCode(positionC, domainC, priority, interfaceInt);
+				}
 			}
 		});
 	}
@@ -141,7 +197,6 @@ public class AppPanel extends JPanel
 		layout.putConstraint(SpringLayout.NORTH, textScrollPane, 50, SpringLayout.NORTH, this);
 		layout.putConstraint(SpringLayout.WEST, textScrollPane, 50, SpringLayout.EAST, switchList);
 		layout.putConstraint(SpringLayout.SOUTH, textScrollPane, 0, SpringLayout.SOUTH, userDomain);
-		layout.putConstraint(SpringLayout.EAST, textScrollPane, -50, SpringLayout.EAST, this);
 		layout.putConstraint(SpringLayout.WEST, resetButton, 10, SpringLayout.EAST, copyButton);
 		layout.putConstraint(SpringLayout.NORTH, userDomain, 50, SpringLayout.SOUTH, switchNumbers);
 		layout.putConstraint(SpringLayout.WEST, userDomain, 0, SpringLayout.WEST, switchList);
@@ -158,7 +213,5 @@ public class AppPanel extends JPanel
 		layout.putConstraint(SpringLayout.SOUTH, stackLabel, -10, SpringLayout.NORTH, switchNumbers);
 		layout.putConstraint(SpringLayout.WEST, domainLabel, 50, SpringLayout.WEST, this);
 		layout.putConstraint(SpringLayout.SOUTH, domainLabel, -10, SpringLayout.NORTH, userDomain);
-		
-		codePane.setText("Hello World!");
 	}
 }
