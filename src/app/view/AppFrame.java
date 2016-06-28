4c246759f89a80dc5a8c6e2c291b0f8125a4037a
Sam Montoya <samuel.montoya@080n-pdc-09.local>
2016-06-28 15:59:38 -0600

    Fixed background

--- src/app/view/AppFrame.java
+++ src/app/view/AppFrame.java
@@ -1,5 +1,9 @@
 package app.view;
 
+import java.awt.Image;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class AppFrame extends JFrame
@@ -9,6 +13,11 @@ public class AppFrame extends JFrame
 	public AppFrame()
 	{
 		panel = new AppPanel();
+		
+		URL iconURL = getClass().getResource("/resources/netLogo.gif");
+		// iconURL is null when not found
+		ImageIcon icon = new ImageIcon(iconURL);
+		this.setIconImage(icon.getImage());
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
