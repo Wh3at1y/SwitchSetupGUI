ccbc56117e551fffffed3266a471846cae4e2a62
Sam Montoya <samuel.montoya@080n-pdc-09.local>
2016-07-05 08:07:50 -0600

    added more buttons

--- src/app/view/TabPanel.java
+++ src/app/view/TabPanel.java
@@ -5,10 +5,14 @@ import javax.swing.*;
 public class TabPanel extends JPanel
 {
 	private JButton irfButton;
+	private JButton snmpButton;
+	private JButton linkaggButton;
 	
 	public TabPanel()
 	{
-		irfButton = new JButton("IRF Setup");
+		irfButton = new JButton("IRF Setup Script");
+		snmpButton = new JButton("SNMP Setup Script");
+		linkaggButton = new JButton("Link Agg Script");
 		
 		buildPanel();
 		buildPlacements();
@@ -17,8 +21,24 @@ public class TabPanel extends JPanel
 
 	private void buildPanel()
 	{
-		this.setLayout(new SpringLayout());
+		SpringLayout springLayout = new SpringLayout();
+		springLayout.putConstraint(SpringLayout.NORTH, linkaggButton, 20, SpringLayout.SOUTH, snmpButton);
+		springLayout.putConstraint(SpringLayout.WEST, linkaggButton, 10, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, linkaggButton, 68, SpringLayout.SOUTH, snmpButton);
+		springLayout.putConstraint(SpringLayout.EAST, linkaggButton, 0, SpringLayout.EAST, irfButton);
+		springLayout.putConstraint(SpringLayout.NORTH, snmpButton, 20, SpringLayout.SOUTH, irfButton);
+		springLayout.putConstraint(SpringLayout.WEST, snmpButton, 10, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, snmpButton, 68, SpringLayout.SOUTH, irfButton);
+		springLayout.putConstraint(SpringLayout.EAST, snmpButton, 0, SpringLayout.EAST, irfButton);
+		springLayout.putConstraint(SpringLayout.NORTH, irfButton, 50, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, irfButton, 10, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, irfButton, 98, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, irfButton, -10, SpringLayout.EAST, this);
+		this.setLayout(springLayout);
+		this.setOpaque(false);
 		add(this.irfButton);
+		add(this.snmpButton);
+		add(this.linkaggButton);
 	}
 
 	private void buildListeners()
